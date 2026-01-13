8fb3a9caac7f0471934dfd5427c266c6db2aaea8
SuchPigeon <kurynovv@yandex.ru>
2026-01-13 22:31:56 +0500

    DAGs re-structrized for triggers DAGs

--- dags/main.py
+++ dags/main.py
@@ -3,6 +3,7 @@ from airflow.models import Variable
 
 import pendulum
 from datetime import datetime, timedelta
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from api.moex_stats import get_securities_info, save_json_data
 from datawarehouse.dwh import staging_table, core_table
 from dataquality.soda import moex_elt_data_quality
@@ -22,39 +23,48 @@ default_args = {
 staging_schema = "staging"
 core_schema = "core"
 
+# Dag step 1: security_info
 with DAG(
         dag_id='security_info',
         default_args=default_args,
         description='DAG to get security info',
         schedule='0 14 * * *',
         catchup=False
-) as dag:
+) as dag_security:
     security_info = get_securities_info(Variable.get('security_name'), Variable.get('date_begin'), Variable.get('date_end'))
     json_data = save_json_data(security_info)
 
-    security_info >> json_data
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db"
+    )
+
+    security_info >> json_data >> trigger_update_db
 
 
-
+# Dag step 2: update_db
 with DAG(
         dag_id='update_db',
         default_args=default_args,
         description='Update db info',
-        schedule='0 15 * * *',
         catchup=False
 ) as dag:
     update_staging = staging_table()
     update_core = core_table()
 
-    update_staging >> update_core
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality"
+    )
+
+    update_staging >> update_core >> trigger_data_quality
 
 
-
+# Dag step 3: data_quality
 with DAG(
         dag_id='data_quality',
         default_args=default_args,
         description='DAG to check the data quality on both layers in the db',
-        schedule='0 16 * * *',
         catchup=False
 ) as dag:
     soda_validate_staging = moex_elt_data_quality(staging_schema)
